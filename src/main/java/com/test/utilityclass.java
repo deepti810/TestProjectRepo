79626388117b5ee35e38a0e709fd5b8291bf05fd
Deepti Sharma <deepti.sharma@algoworks.com>
2019-10-17 19:22:16 +0530

    adding for rebasing

--- src/main/java/com/test/utilityclass.java
+++ src/main/java/com/test/utilityclass.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+//this is a utility class
 public class utilityclass {
 	
 	
