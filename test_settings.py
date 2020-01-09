c94f62c0652cbd158fd08f7a01cf3a85d8909451
Deepti Sharma <deepti.sharma@algoworks.com>
2020-01-09 16:47:17 +0530

    changed yml and created settings file

---
+++ test_settings.py
@@ -0,0 +1,43 @@
+import os
+
+from selenium import webdriver
+
+SELENIUM_WEBDRIVERS = {
+    "default": {"callable": webdriver.Chrome, "args": (), "kwargs": {}},
+    "firefox": {"callable": webdriver.Firefox, "args": (), "kwargs": {}},
+}
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "portal.context_processors.process_newsletter_form",
+            ]
+        },
+    }
+]
+
+if os.environ.get('SELENIUM_HEADLESS', None):
+    from pyvirtualdisplay import Display
+    display = Display(visible=0, size=(1624, 1024))
+    display.start()
+    import atexit
+    atexit.register(lambda: display.stop())
+
+INSTALLED_APPS = ["portal"]
+PIPELINE_ENABLED = False
+ROOT_URLCONF = "example_project.example_project.urls"
+STATIC_ROOT = "example_project/example_project/static"
+SECRET_KEY = "bad_test_secret"
+
+from django_autoconfig.autoconfig import configure_settings
+
+configure_settings(globals())
